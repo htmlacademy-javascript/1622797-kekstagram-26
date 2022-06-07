0acec2218d1e48f7c4355c3454fbf4d24587c21f
Kirill Podryadov <iamkir90@hotmail.com>
2022-06-07 14:05:06 +0300

    Исправил ошибку

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 // Функция, возвращающая случайное целое число из переданного диапазона
 function getRandomPositiveInteger (a, b) {
-	const lowwer = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+	const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
 	const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
 
-	const result = Math.random() * (upper - lowwer + 1) + lowwer;
+	const result = Math.random() * (upper - lower + 1) + lower;
 
 	return Math.floor(result);
 }
