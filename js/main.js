2d8e518c4bfcbfb305225af7a9052912cd6e29bb
Kirill Podryadov <iamkir90@hotmail.com>
2022-06-07 14:18:04 +0300

    Поправил отступы

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 // Функция, возвращающая случайное целое число из переданного диапазона
 function getRandomPositiveInteger (a, b) {
-	const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-	const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
 
-	const result = Math.random() * (upper - lower + 1) + lower;
+  const result = Math.random() * (upper - lower + 1) + lower;
 
-	return Math.floor(result);
+  return Math.floor(result);
 }
 
 getRandomPositiveInteger(2, 5);
@@ -13,7 +13,7 @@ getRandomPositiveInteger(2, 5);
 
 // Функция для проверки максимальной длины строки
 function checkStringLength (string, maxLength) {
-	return string.length <= maxLength;
+  return string.length <= maxLength;
 }
 
 checkStringLength ('Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 140);
